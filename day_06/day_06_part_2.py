3625d26a657f2de034b794b3dfb7c6262a9b167b
Skyrmo <owaingskyrme@gmail.com>
2025-12-15 23:49:42 +0800

    Completed Day 6, part 1 & 2.

--- day_06/day_06_part_2.py
+++ day_06/day_06_part_2.py
@@ -1,15 +1,16 @@
-
-import os
 import collections
+import os
+import re
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,16 +20,38 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    print(input_data)
+    grid = [[0] * 1000 for _ in range(1000)]
+    result = 0
+
+    for line in input_data:
+        regex = r"(toggle|turn on|turn off)\s(\d*),(\d*)\sthrough\s(\d*),(\d*)"
+        action, r1, c1, r2, c2 = re.findall(regex, line)[0]
+
+        for r in range(int(r1), int(r2) + 1):
+            for c in range(int(c1), int(c2) + 1):
+                if action == "toggle":
+                    grid[r][c] += 2
+                elif action == "turn on":
+                    grid[r][c] += 1
+                elif action == "turn off":
+                    grid[r][c] = max(0, grid[r][c] - 1)
+
+    for r in range(1000):
+        for c in range(1000):
+            result += grid[r][c]
+
+    return result
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, 'sample_input.txt')
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +60,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 06, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
