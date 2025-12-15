92ee441c7291caac3ba05600d7796a005aef5c8c
Skyrmo <owaingskyrme@gmail.com>
2025-12-15 16:42:01 +0800

    Completed Day 2 parts 1 & 2.

--- day_02/day_02_part_2.py
+++ day_02/day_02_part_2.py
@@ -1,15 +1,14 @@
-
 import os
-import collections
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,16 +18,29 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    print(input_data)
+    result = 0
+    for line in input_data:
+        split_line = line.split("x")
+        w, h, l = int(split_line[0]), int(split_line[1]), int(split_line[2])
+        side_1 = 2 * w + 2 * h
+        side_2 = 2 * h + 2 * l
+        side_3 = 2 * w + 2 * l
+
+        area = w * h * l
+        result += min(side_1, side_2, side_3) + area
+
+    return result
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +49,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 02, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
