61ff436ca6e9d4a9919b6f9a9a1f5b00f0c81989
Skyrmo <owaingskyrme@gmail.com>
2025-12-19 00:22:41 +0800

    Completed Day 17, part 1 & 2.

--- day_17/day_17_part_1.py
+++ day_17/day_17_part_1.py
@@ -1,15 +1,16 @@
-
-import os
 import collections
+import os
+from functools import cache
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,16 +20,50 @@ def parse_input(file_path):
 
         return data
 
+
+valid_combos = set()
+
+
+def dp(containers, idx, eggnog, path):
+    if eggnog == 0:
+        if path not in valid_combos:
+            valid_combos.add(str(path))
+            return 1
+
+    if eggnog < 0:
+        return 0
+
+    if eggnog > 0 and not containers:
+        return 0
+
+    result = 0
+    for i in range(idx, len(containers)):
+        new_container, id = containers[i]
+
+        new_path = path.copy()
+        new_path.add((new_container, id))
+
+        result += dp(containers, i + 1, eggnog - new_container, new_path)
+
+    return result
+
+
 def solve(input_data):
-    print(input_data)
+    containers = []
+
+    for container in input_data:
+        containers.append(int(container))
+
+    return dp(containers, 0, 150, set())
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +72,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 17, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- day_17/day_17_part_2.py
+++ day_17/day_17_part_2.py
@@ -1,15 +1,14 @@
-
 import os
-import collections
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,16 +18,45 @@ def parse_input(file_path):
 
         return data
 
+
+def dp(containers, idx, eggnog, path):
+    if eggnog == 0:
+        return len(path)
+
+    if eggnog < 0:
+        return float("inf")
+
+    if eggnog > 0 and not containers:
+        return float("inf")
+
+    result = float("inf")
+    for i in range(idx, len(containers)):
+        new_container, id = containers[i]
+
+        new_path = path.copy()
+        new_path.add((new_container, id))
+
+        result = min(result, dp(containers, i + 1, eggnog - new_container, new_path))
+
+    return result
+
+
 def solve(input_data):
-    print(input_data)
+    containers = []
+
+    for i, container in enumerate(input_data):
+        containers.append((int(container), i))
+
+    return dp(containers, 0, 150, set())
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +65,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 17, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
