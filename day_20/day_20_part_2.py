a48ddf232639805827916e801ddc47162428dc4b
Skyrmo <owaingskyrme@gmail.com>
2025-12-19 15:37:21 +0800

    Completed Day 20, Part 1 & 2.

--- day_20/day_20_part_2.py
+++ day_20/day_20_part_2.py
@@ -1,10 +1,10 @@
-
-import os
 import collections
+import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
@@ -19,16 +19,43 @@ def parse_input(file_path):
 
         return data
 
+
+# use math to calculate all of the factors of each number
+def calc_for_num(num):
+    factors = []
+    for i in range(1, int(num**0.5) + 1):
+        if num % i == 0:
+            factors.append(i)
+            if i != num // i:
+                factors.append(num // i)
+
+    result = 0
+    for factor in factors:
+        if num <= factor * 50:
+            result += factor * 11
+
+    return result
+
+
 def solve(input_data):
-    print(input_data)
+    target = int(input_data)
+
+    # 7.55 sec
+    i = 0
+    while True:
+        result = calc_for_num(i)
+        if result >= target:
+            return i
+        i += 2
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +64,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 20, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
