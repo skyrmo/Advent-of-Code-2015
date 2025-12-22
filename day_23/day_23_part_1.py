8e8ca9b420e5415057f378680454f5b2af1c43ba
Skyrmo <owaingskyrme@gmail.com>
2025-12-23 00:27:04 +0800

    Completed Day 23, parts 1 & 2.

--- day_23/day_23_part_1.py
+++ day_23/day_23_part_1.py
@@ -1,15 +1,15 @@
-
 import os
-import collections
+import re
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,16 +19,61 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    print(input_data)
+    instructions = []
+    for line in input_data:
+        m = re.search(r"\b([ab])\b", line)
+        n = re.search(r"([+-]\d+)", line)
+
+        instruction = line[:3]
+        register = m.group(1) if m else None
+        jump = n.group(0) if n else None
+
+        # print(line)
+        # print(instruction, register, jump)
+        instructions.append((instruction, register, jump))
+
+    idx = 0
+    n = len(instructions)
+    registers = {"a": 0, "b": 0}
+
+    while idx < n:
+        instruction, reg, jump = instructions[idx]
+        # print(idx, instruction, register, jump)
+
+        if instruction == "hlf":
+            registers[reg] //= 2
+        elif instruction == "tpl":
+            registers[reg] *= 3
+        elif instruction == "inc":
+            registers[reg] += 1
+        elif instruction == "jmp":
+            idx += int(jump)
+            continue
+        elif instruction == "jie":
+            if registers[reg] % 2 == 0:
+                idx += int(jump)
+                continue
+        elif instruction == "jio":
+            if registers[reg] == 1:
+                idx += int(jump)
+                continue
+        else:
+            print("Invalid instruction")
+
+        idx += 1
+
+    return registers["b"]
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +82,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 23, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
