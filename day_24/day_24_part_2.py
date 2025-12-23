8fdf63b51c39f108fcb2f816e6233bdd2c068627
Skyrmo <owaingskyrme@gmail.com>
2025-12-23 23:22:51 +0800

    Completed Day 24, Parts 1 & 2.

--- day_24/day_24_part_2.py
+++ day_24/day_24_part_2.py
@@ -1,10 +1,12 @@
-
-import os
 import collections
+import math
+import os
+from itertools import combinations
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
@@ -12,23 +14,43 @@ def parse_input(file_path):
         # return data.split('\n')
 
         # 3. Read as a list of integers
-        # return [int(line) for line in data.split('\n')]
+        return [int(line) for line in data.split("\n")]
 
         # 4. Read as a list of lists (e.g., for grid-like inputs)
         # return [list(line) for line in data.split('\n')]
 
         return data
 
-def solve(input_data):
-    print(input_data)
+
+def solve(nums):
+    n = len(nums)
+    tgt = sum(nums) // 4
+    valid_combos = []
+    found = False
+
+    for i in range(n):
+        valid_combos = []
+
+        for combo in combinations(nums, i):
+            if sum(combo) == tgt:
+                valid_combos.append((combo, math.prod(combo)))
+                found = True
+
+        if found:
+            break
+
+    valid_combos.sort(key=lambda x: x[1])
+
+    return valid_combos[0][1]
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +59,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 24, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
